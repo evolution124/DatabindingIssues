61e38c4c285826698b4ee537b0d1330e6e42274a
Brandon Hubbard <brandon.hubbard@gmail.com>
2013-01-10 09:42:53 -0500

    Adding sample projects

---
+++ PanoramaSelectionChanged/PanoramaSelectionChanged/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using PanoramaSelectionChanged.Resources;
+
+namespace PanoramaSelectionChanged
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
